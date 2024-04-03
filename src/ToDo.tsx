33842db87e57bc86a4c7cd9ba3f5db7e8586b091
Joe Pietruch <jcpigm@rit.edu>
2024-04-03 00:53:39 -0400

    Use Action Props

--- src/ToDo.tsx
+++ src/ToDo.tsx
@@ -54,21 +54,25 @@ export interface ToDoProps {
   title?: string;
   description?: string;
   complete?: boolean;
+  toggleAction: () => void;
+  editAction: () => void;
 }
 
 export const ToDo: FC<ToDoProps> = ({
   title = 'ToDo Item',
   description = 'ToDo Description',
   complete = false,
+  toggleAction,
+  editAction,
 }) => {
   return (
     <li>
-      <input type="checkbox" checked={complete} />
+      <input type="checkbox" checked={complete} onChange={toggleAction} />
       <div>
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
-      <button>{'✏️'}</button>
+      <button onClick={editAction}>{'✏️'}</button>
     </li>
   );
 };
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useRef, useState } from 'react';
 import { ToDosAPI } from './ToDosAPI';
 import { ToDo as ToDoType } from './ToDosAPI/ToDo.type';
+import { ToDo } from './ToDo';
 
 export const ToDoList: FC = () => {
   const [toDos, setToDos] = useState<ToDoType[]>([]);
@@ -8,6 +9,12 @@ export const ToDoList: FC = () => {
 
   const [title, setTitle] = useState('');
   const descriptionRef = useRef<HTMLInputElement>(null);
+
+  const editDialogRef = useRef<HTMLDialogElement>(null);
+
+  const [editId, setEditId] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const editDescriptionRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const fetchToDos = async () => {
@@ -21,7 +28,24 @@ export const ToDoList: FC = () => {
   return (
     <>
       <h1>ToDos:</h1>
-      <ul></ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo
+            {...toDo}
+            key={toDo.id}
+            toggleAction={async () => {
+              await ToDosAPI.update.toggle(toDo.id, toDo.complete);
+              setToDos(await ToDosAPI.read());
+            }}
+            editAction={async () => {
+              setEditId(toDo.id);
+              setEditTitle(toDo.title);
+              editDescriptionRef.current!.value = toDo.description;
+              editDialogRef.current?.showModal();
+            }}
+          />
+        ))}
+      </ul>
       <button
         id="add"
         onClick={async () => {
@@ -69,6 +93,48 @@ export const ToDoList: FC = () => {
             }}
           >
             Create To-Do
+          </button>
+        </form>
+      </dialog>
+
+      <dialog id="editDialog" ref={editDialogRef}>
+        <h1>Edit ToDo:</h1>
+        <form method="dialog">
+          <div>
+            <label>Title</label>
+            <input
+              id="editTitle"
+              type="text"
+              name="title"
+              placeholder="enter a title"
+              value={editTitle}
+              onChange={(event) => {
+                setEditTitle(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label>Description</label>
+            <input
+              id="editDescription"
+              name="description"
+              type="text"
+              placeholder="enter a description"
+              ref={editDescriptionRef}
+            />
+          </div>
+          <input type="hidden" id="editId" name="id" />
+          <button
+            onClick={async () => {
+              await ToDosAPI.update.edit(editId, {
+                title: editTitle || 'ToDo Title',
+                description:
+                  editDescriptionRef.current?.value || 'ToDo Description',
+              });
+              setToDos(await ToDosAPI.read());
+            }}
+          >
+            Edit To-Do
           </button>
         </form>
       </dialog>
