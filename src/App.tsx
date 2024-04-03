633609d5b7c58191f575fb6a1a56315cdc1b4cc1
Joe Pietruch <jcpigm@rit.edu>
2024-04-03 03:17:53 -0400

    Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ToDoList } from './ToDoList';
 function App() {
   return (
     <main>
+      {/* It's nice to hide complexity inside a component. */}
       <ToDoList />
     </main>
   );
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -3,8 +3,18 @@ import { ToDosAPI } from './ToDosAPI';
 import { ToDo as ToDoType } from './ToDosAPI/ToDo.type';
 
 export const ToDoList: FC = () => {
+  // The useState hook takes an initial value
+  // and returns [currentValue, setterFunction].
+  // In this case:
+  // toDos - always contains the most recent ToDo objects
+  // setToDos - can be called to update the value of toDos
   const [toDos, setToDos] = useState<ToDoType[]>([]);
 
+  // The useEffect hook runs any time one of its dependencies changes.
+  // Since this particular one has an empty dependency array (see [] on line 23 below)
+  // it only runs once the first time the component renders.
+  // In this case:
+  // It reads the ToDos from localstorage, and updates the value of toDos with setToDos.
   useEffect(() => {
     const fetchToDos = async () => {
       setToDos(await ToDosAPI.read());
