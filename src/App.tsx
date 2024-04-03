b492623510a5c247a7bd2d2cd97bd0dc3e6e581b
Joe Pietruch <jcpigm@rit.edu>
2024-04-03 03:05:50 -0400

    Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { ToDo } from './ToDo';
 
 function App() {
   return (
+    // we can do inline CSS, numbers are automatically considered pixels
     <main style={{ padding: 40 }}>
       <h1>Hello, React</h1>
       <ul>
+        {/* ToDo is a component. This one has no props. */}
         <ToDo />
+
+        {/* This instance of ToDo has props : title, description, and complete */}
         <ToDo
           title="Title Prop"
           description="Description Prop"
