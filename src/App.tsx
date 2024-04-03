3d537f5c9b35de492dfd02a5659ec1da2b41a85e
Joe Pietruch <jcpigm@rit.edu>
2024-04-02 23:30:50 -0400

    JSX/TSX and Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { ToDo } from './ToDo';
+
 function App() {
   return (
-    <>
+    <main style={{ padding: 40 }}>
       <h1>Hello, React</h1>
-    </>
+      <ul>
+        <ToDo />
+        <ToDo
+          title="Title Prop"
+          description="Description Prop"
+          complete={true}
+        />
+      </ul>
+    </main>
   );
 }
 
